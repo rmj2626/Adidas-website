12da7a5592088e7369db5ae63990230c4d9ad770
rmj2626 <rudraksha.jejurikar21@pccoe.org>
2024-09-25 15:05:30 +0530

    95

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || 8000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
+});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -11,7 +14,18 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Sample user storage (replace with a database in a real application)
-let users = [];
+let users = [
+  {
+    username: "RMJ20",
+    email: "rudraksha.jejurikar21@pccoepune.org",
+    password: "abcd1234"
+  },
+  {
+    username: "Ram",
+    email: "r@pm.me",
+    password: "RAM"
+  }
+];
 
 // Login page route (GET)
 app.get("/login", (req, res) => {
